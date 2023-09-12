4cfcff294a89e296c80e0b61fecc73e9f538ee3a
Allan Kiplagat <allankiplagatkipkemei@gmail.com>
2023-09-12 16:43:11 +0300

    added some static files

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,6 +1,6 @@
 import slide01 from '../static/slide01.jpg'
 import slide02 from '../static/slide02.jpg'
-import slide03 from '../static/slide03.jpg'
+import slide03 from '../static/slide03.png'
 
 import Carousel from 'react-bootstrap/Carousel';
 
